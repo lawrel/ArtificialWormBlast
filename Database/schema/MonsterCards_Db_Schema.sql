c500efaa283f29182968b187df0095f2053e2cf8
jkloman11 <jkloman11@gmail.com>
2019-04-11 18:46:21 -0400

    add cards table

--- Database/schema/MonsterCards_Db_Schema.sql
+++ Database/schema/MonsterCards_Db_Schema.sql
@@ -1,11 +1,11 @@
 /* 	Monster Cards Db Schema
- 
+
 	Authors: Sean Rice, (add your name here)
-    
+
     Proposed Schema:
 		Users(PK_ID, First, MidInit, Last, Email)
         UserLogins(PK_AuthToken, FK_AccountID, ExpirationDate)
-        
+
         Card(ID*, Name, DoodleData)
 */
 drop table Users;
@@ -21,7 +21,7 @@ create table Users(
     Last varchar(127),
     Email varchar(255) not null,
     Password char(64) not null,
-    
+
     primary key (ID),
     unique (Email)
 );
@@ -32,14 +32,38 @@ create table UserLogins(
 	AccountID int not null,
 	AuthToken varchar(255),
     ExpirationDate datetime not null,
-    
+
     primary key (AccountID),
     unique (AuthToken),
-    
+
     FOREIGN KEY (AccountID)
         REFERENCES Users(ID)
         ON DELETE CASCADE
         ON UPDATE CASCADE
+);
+
+create table Cards(
+	ID int auto_increment,
+	CardName varchar(256),
+	ImgData BLOB not null,
+
+	primary key (CardID)
+);
+
+create table PlayerCards(
+	PlayerID int,
+	CardID int
+
+	foreign key (PlayerID),
+	foreign key (CardID)
+
+	REFERENCES Cards(ID)
+	ON DELETE CASCADE
+    ON UPDATE CASCADE
+
+	REFERENCES Users(ID)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
 );
 
 begin;
