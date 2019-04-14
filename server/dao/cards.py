b524e24637a8f6d277f91eb00a319cbc65b863c1
jkloman11 <jkloman11@gmail.com>
2019-04-14 18:30:44 -0400

    pictures from db

--- server/dao/cards.py
+++ server/dao/cards.py
@@ -28,7 +28,6 @@ class BadFileExtError(Error):
     pass
 
 
-
 def getPlayerDeck(playerId):
     query = """
             select CardID from MonsterCards.UsersCards
@@ -37,14 +36,13 @@ def getPlayerDeck(playerId):
     cards = execute(query, (playerId, ))
     return [CardID for CardID in cards]
 
+
 def addPlayerCard(playerId, cardId):
     query = """
             INSERT INTO MonsterCards.UsersCards (UserID, CardID) VALUES (%s, %s);
             """
     execute(query, (playerId, cardId))
-
-
-
+    
 
 def convertToBinaryData(filename):
     # Convert digital data to binary format
